922e65c1bb48c45c1f5d2cd8160350a3e78e5dd6
Renzo Alamo <renzoalamo@hotmail.com>
2022-05-31 15:06:24 -0500

    change index.php 6

--- Config/autoload.php
+++ Config/autoload.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . "/init.php";
 
 spl_autoload_register(function ($class) {
-  print_r($class);
   $file = root . "/" . str_replace("\\", "/", $class) . ".php";
   if (file_exists($file)) {
+    print_r($file);
     require_once $file;
   }
 });
