6211ef35714b130938f83f2cfc070126a022e0ce
Renzo Alamo <renzoalamo@hotmail.com>
2022-06-03 21:03:12 -0500

    change prefix 57

--- Core/Router/Route.php
+++ Core/Router/Route.php
@@ -154,9 +154,8 @@ class Route
       array_push($params, $param);
       return $generic_regex;
     }, $path);
-    self::$routes["$method → $path"] = [
+    self::$routes["$method → $original_path"] = [
       "method" => $method,
-      "original_path" => $original_path,
       "path" => $path,
       "action" => $action,
       "params" => count($params) > 0 ? array_reduce($params, function ($previous, $current) use ($generic_regex) {
@@ -169,15 +168,15 @@ class Route
         !isset(self::$routes["$method → $path"]["params"][$param]) ||
         (!is_string($regex) || strlen(trim($regex)) === 0 || preg_match("~$regex~", "") === false)
       ) return;
-      // self::$routes["$method → $path"]["params"][$param] = $regex;
-      self::$routes = array_reduce(self::$routes, function ($previous, $current) use ($method, $path, $param, $regex) {
-        if ($current["method"] === $method && $current["path"] === $path) {
-          $path = preg_replace("/\{$param\}/", "($regex)", $current["original_path"]);
-          $current["params"][$param] = $regex;
-        }
-        $previous["$method → $path"] = $current;
-        return $previous;
-      }, []);
+      self::$routes["$method → $path"]["params"][$param] = $regex;
+      // self::$routes = array_reduce(self::$routes, function ($previous, $current) use ($method, $path, $param, $regex) {
+      //   if ($current["method"] === $method && $current["path"] === $path) {
+      //     $path = preg_replace("/\{$param\}/", "($regex)", $current["original_path"]);
+      //     $current["params"][$param] = $regex;
+      //   }
+      //   $previous["$method → $path"] = $current;
+      //   return $previous;
+      // }, []);
     };
     return new Validate($changeRegex);
   }
